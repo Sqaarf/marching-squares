43f3598acca62c1b74e9bd5713003bfd13a0567a
zsabbah <zsabbah@lr-univ.fr>
2022-03-13 22:51:58 +0100

    Changed hue calculation

--- v1/Point.py
+++ v1/Point.py
@@ -8,7 +8,7 @@ class Point:
         self.x = x
         self.y = y
         self.v = v
-        self.hue = int(self.v * 255)
+        self.hue = int((self.v % int(self.v)) * 255)
         self.color = (self.hue, self.hue, self.hue)
         self.font = pygame.font.SysFont("times new roman", 10)
 
@@ -18,7 +18,7 @@ class Point:
         scalar_label = self.font.render("%.3f" % self.v, True, (255,255,255))
 
         pygame.draw.circle(win, self.color, (self.x, self.y), 2)
-        win.blit(scalar_label, (self.x + scalar_label.get_width() // 2, self.y))
+        win.blit(scalar_label, (self.x + scalar_label.get_width() // 2 - 6, self.y))
 
     def __str__(self):
         return f"({self.x},{self.y})"
