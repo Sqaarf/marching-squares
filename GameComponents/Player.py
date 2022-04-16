b174233a986b915f7e87b3eb0e090cb240879de6
Sqaarf <mrscarf19@gmail.com>
2022-04-16 22:43:47 +0200

    Indent trim

--- GameComponents/Player.py
+++ GameComponents/Player.py
@@ -3,12 +3,12 @@ import pygame
 class Player:
     def __init__(self, name):
         self.name = name
-        self.x = 0
+        self.x = 3
         self.y = 0
         self.color = (255, 0, 0)
     
     def render(self, win):
-         pygame.draw.circle(win, self.color, (self.x, self.y), 3)
+        pygame.draw.circle(win, self.color, (self.x, self.y), 3)
 
     def __str__(self) -> str:
         return f"{self.name}:({self.x}, {self.y})"
