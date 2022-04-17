e7d26672fc562454c0329e3c927bd3aee968aa95
Sqaarf <mrscarf19@gmail.com>
2022-04-17 15:02:13 +0200

    Added isColliding function

--- GameComponents/Player.py
+++ GameComponents/Player.py
@@ -6,9 +6,19 @@ class Player:
         self.x = 3
         self.y = 3
         self.color = (255, 0, 0)
+        self.rect = pygame.Rect(self.x, self.y, 3, 3)
     
     def render(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), 3)
+        self.rect = pygame.Rect(self.x, self.y, 3, 3)
+    
+    def isColliding(self, walls):
+        # for wall in walls:
+        #     if pygame.Rect.colliderect(self.rect, wall):
+        #         return True
+        # return False
+
+        return pygame.Rect.collidelistall(self.rect, walls)
 
     def __str__(self) -> str:
         return f"{self.name}:({self.x}, {self.y})"
