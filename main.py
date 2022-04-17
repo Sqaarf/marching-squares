eef5d1d58f126d579b31e38a9ef853acc19535c8
Sqaarf <mrscarf19@gmail.com>
2022-04-17 15:00:24 +0200

    Changed verbose to debug by key press

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ from GameComponents.Player import Player
 
 if __name__ == "__main__":
     pygame.init()
-
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "--verbose":
-            verbose = True
-        else:
-            verbose = False
-    else:
-        verbose = False
-    window = Window(Player("Sqaarf"), verbose)
+    
+    window = Window(Player("Sqaarf"))
     window.loop()
 
     pygame.quit()
